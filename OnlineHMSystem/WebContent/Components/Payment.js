fa5c73e90ecfc8ae5474d3b471e266a97cae0186
Hasindu Dilshan <53825832+HasinduDilshan@users.noreply.github.com>
2020-05-06 21:09:04 +0530

    final .js code

--- OnlineHMSystem/WebContent/Components/Payment.js
+++ OnlineHMSystem/WebContent/Components/Payment.js
@@ -1,9 +1,6 @@
 $(document).ready(function()
 {
-if ($("#alertSuccess").text().trim() == "")
- {
  $("#alertSuccess").hide();
- }
  $("#alertError").hide();
 });
 // SAVE ============================================
