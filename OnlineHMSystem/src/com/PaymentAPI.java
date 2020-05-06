a1018e7b52633248140f77c2507975e4a3406c04
Hasindu Dilshan <53825832+HasinduDilshan@users.noreply.github.com>
2020-05-06 21:14:58 +0530

    update

--- OnlineHMSystem/src/com/PaymentAPI.java
+++ OnlineHMSystem/src/com/PaymentAPI.java
@@ -1,16 +1,16 @@
 package com;
 
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.Payment;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
 
 /**
  * Servlet implementation class PaymentAPI
