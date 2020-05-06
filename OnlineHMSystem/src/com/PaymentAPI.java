275a239297a941f57f3e37a4efa1dad32a2dacd6
Msi <Msi@DESKTOP-4DUI4T5>
2020-05-06 16:16:55 +0530

    Add payment API savelet

--- OnlineHMSystem/src/com/PaymentAPI.java
+++ OnlineHMSystem/src/com/PaymentAPI.java
@@ -6,7 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import model.Payment;
+import com.Payment;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
