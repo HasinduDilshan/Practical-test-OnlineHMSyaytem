52cfb723e6a235eb89e542b04a8058ad12213a40
Msi <Msi@DESKTOP-4DUI4T5>
2020-05-06 15:12:12 +0530

    components added

---
+++ OnlineHMSystem/WebContent/Components/Payment.js
@@ -0,0 +1,78 @@
+$(document).ready(function()
+{
+if ($("#alertSuccess").text().trim() == "")
+ {
+ $("#alertSuccess").hide();
+ }
+ $("#alertError").hide();
+});
+// SAVE ============================================
+$(document).on("click", "#btnSave", function(event)
+{
+// Clear alerts---------------------
+ $("#alertSuccess").text("");
+ $("#alertSuccess").hide();
+ $("#alertError").text("");
+ $("#alertError").hide();
+// Form validation-------------------
+var status = validatePaymentForm();
+if (status != true)
+ {
+ $("#alertError").text(status);
+ $("#alertError").show();
+ return;
+ }
+// If valid------------------------
+ $("#formBillinfo").submit();
+});
+// UPDATE==========================================
+$(document).on("click", ".btnUpdate", function(event)
+{
+ $("#hidPaymentIDSave").val($(this).closest("tr").find('#hidPaymentIDUpdate').val());
+ $("#PaymentID").val($(this).closest("tr").find('td:eq(0)').text());
+ $("#PayDate").val($(this).closest("tr").find('td:eq(1)').text());
+ $("#CustomerName").val($(this).closest("tr").find('td:eq(2)').text());
+ $("#Amount").val($(this).closest("tr").find('td:eq(3)').text());
+ $("#Description").val($(this).closest("tr").find('td:eq(4)').text());
+});
+// CLIENTMODEL=========================================================================
+function validatePaymentForm()
+{
+// CODE
+if ($("#PaymentID").val().trim() == "")
+ {
+ return "Insert Paymen Code.";
+ }
+// DATE
+if ($("#PayDate").val().trim() == "")
+ {
+ return "Insert Date.";
+ } 
+// NAME
+if ($("#CustomerName").val().trim() == "")
+{
+return "Insert Customer Name.";
+}
+//PRICE-------------------------------
+if ($("#Amount").val().trim() == "")
+ {
+ return "Insert Item Price.";
+ }
+// is numerical value
+var tmpPrice = $("#Amount").val().trim();
+if (!$.isNumeric(tmpPrice))
+ {
+ return "Insert a numerical value for Amount.";
+ }
+// convert to decimal price
+ $("#Amount").val(parseFloat(tmpPrice).toFixed(2));
+// DESCRIPTION------------------------
+if ($("#Description").val().trim() == "")
+ {
+ return "Insert Description.";
+ }
+return true;
+}
+
+
+
